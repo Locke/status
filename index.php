3f3fbe6bff293894484e8ca87e9fb9a2fca7210a
levu <levu@alderaan.scienceco.de>
2012-07-14 14:02:55 +0200

    footer changed

--- index.php
+++ index.php
@@ -202,8 +202,9 @@ echo '
        <div class="container">
          <p class="pull-right"><a href="#">zurück nach oben</a></p>
          <p>
-           Designed and built with all the love in the world <a href="http://twitter.com/twitter" target="_blank">@twitter</a> by <a href="http://twitter.com/mdo" target="_blank">@mdo</a> and <a href="http://twitter.com/fat" target="_blank">@fat</a>.<br />
-           Code licensed under the <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License v2.0</a>.
+				 	Designed and built with all the love in the world <a href="http://twitter.com/twitter" target="_blank">@twitter</a> by <a href="http://twitter.com/mdo" target="_blank">@mdo</a> and <a href="http://twitter.com/fat" target="_blank">@fat</a>. <br />
+					Hessische Adaption von <a href="http://twitter.com/levudev">@levudev</a><br>
+          Code licensed under the <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License v2.0</a>.
          </p>
        </div>
      </footer>
