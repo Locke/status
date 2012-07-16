0529439937936866a0628998e3a8fdb3b1030cf6
levu <levu@alderaan.scienceco.de>
2012-07-16 15:35:34 +0200

    small HTML fix

--- mailman-stats.php
+++ mailman-stats.php
@@ -2,7 +2,7 @@
 <?php
 	$content = file_get_contents('http://lists.piratenpartei-hessen.de/stats/stats.txt');
 	$lines = explode("\n", $content);
-	echo '<table><tr><th>Anzahl an Abonnenten</th><th>Liste</th>';
+	echo '<table><tr><th>Anzahl an Abonnenten</th><th>Liste</th></tr>';
 	foreach ($lines as $line) {
 		$parts = explode(" ", $line, 2);
 		echo '<tr><td>' . $parts[0] . '</td><td>' . $parts[1] . '</td></tr>';
